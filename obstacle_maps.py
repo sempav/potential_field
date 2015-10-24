372662c46b0a2c85688d238e9055c0c878bf9516
urezan <pavel.semashko@gmail.com>
2015-10-24 17:42:15 +0300

    Add a new map

--- obstacle_maps.py
+++ obstacle_maps.py
@@ -17,3 +17,7 @@ maps[1] = [obstacle.create_obstacle_circle(Point(1.25, 0.5), 1.1),
 
 maps[2] = [obstacle.create_obstacle_circle(Point(2 * x, 2 * y), 0.2)
            for x in xrange(-1, 2) for y in xrange(-1, 2)]
+
+maps[3] = [obstacle.create_obstacle_polygon([Point(0.0, 0.0),
+                                             Point(3.0, 1.5),
+                                             Point(3.0, -1.5)])]
